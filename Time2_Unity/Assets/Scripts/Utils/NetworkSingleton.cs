827c854955d84bc28a5f826096230112cd4e5ff8
Thomaz Miranda <43679181+tomytp@users.noreply.github.com>
2022-09-21 23:45:08 -0300

    Basic Utilities

---
+++ Time2_Unity/Assets/Scripts/Utils/NetworkSingleton.cs
@@ -0,0 +1,33 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkSingleton<T> : NetworkBehaviour
+    where T : Component
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var objs = FindObjectsOfType(typeof(T)) as T[];
+                if (objs!.Length > 0)
+                    _instance = objs[0];
+                if (objs.Length > 1)
+                {
+                    Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
+                }
+                if (_instance == null)
+                {
+                    var obj = new GameObject
+                    {
+                        name = $"_{typeof(T).Name}"
+                    };
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Time2_Unity/Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+public class Singleton<T> : MonoBehaviour
+    where T : Component
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var objs = FindObjectsOfType(typeof(T)) as T[];
+                if (objs!.Length > 0)
+                    _instance = objs[0];
+                if (objs.Length > 1)
+                {
+                    Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
+                }
+                if (_instance == null)
+                {
+                    var obj = new GameObject
+                    {
+                        name = $"_{typeof(T).Name}"
+                    };
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+}
