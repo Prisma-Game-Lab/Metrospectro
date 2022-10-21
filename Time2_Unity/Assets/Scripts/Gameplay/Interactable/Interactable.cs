b1e7ba774dab50d9145fbee04f8a6cc93f4f075b
Thomaz Miranda <43679181+tomytp@users.noreply.github.com>
2022-10-20 23:41:09 -0300

    textures

--- Time2_Unity/Assets/Scripts/Gameplay/Interactable/Interactable.cs
+++ Time2_Unity/Assets/Scripts/Gameplay/Interactable/Interactable.cs
@@ -2,19 +2,16 @@ using System;
 using Unity.Netcode;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 public class Interactable : NetworkBehaviour
 {
     private Transform _player;
     private IInteraction _interaction;
     private bool _interacted;
     private Animator _animator;
-    private static readonly int Interacted = Animator.StringToHash("Interacted");
     
     private void Awake()
     {
         _interaction = GetComponent<IInteraction>();
-        //_animator = GetComponent<Animator>();
     }
 
     public void OnInteract()
@@ -25,25 +22,16 @@ public class Interactable : NetworkBehaviour
     [ClientRpc]
     private void HandleInteractionClientRpc()
     {
-        //if (AnimatorIsPlaying()) return;
         
         if (!_interacted)
         {
-            //_animator.SetBool(Interacted, true);
             _interaction.Interact();
         }
         else
         {
             _interaction.ComeBack();
-            //_animator.SetBool(Interacted, false);
         }
         _interacted = !_interacted;
-    }
-        
-
-    private bool AnimatorIsPlaying()
-    {
-        return _animator.GetCurrentAnimatorStateInfo(0).length > _animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
     }
 }
 
