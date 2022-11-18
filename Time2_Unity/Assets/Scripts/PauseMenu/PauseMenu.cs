98a0327997fd55205bc67059a65b024647deb39d
Thomaz Miranda <43679181+tomytp@users.noreply.github.com>
2022-11-18 01:15:24 -0300

    Pause sync

--- Time2_Unity/Assets/Scripts/PauseMenu/PauseMenu.cs
+++ Time2_Unity/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -1,57 +1,100 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : NetworkBehaviour
 {
-    public static bool GameIsPaused = false;
-
     public GameObject pauseMenuUI;
+    private static bool _isPaused;
 
-        // Update is called once per frame
-        void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                if (GameIsPaused)
-                {
-                    Resume();
-                }
-                else
-                {
-                    Pause();
-                }
-            }
-        }
+    private readonly List<InputActionMap> _actionMaps = new();
 
-        public void Resume()
-        {
-            pauseMenuUI.SetActive(false);
-            Time.timeScale = 1f;
-            GameIsPaused = false;
-        }
-        void Pause()
-        {
-            pauseMenuUI.SetActive(true);
-            Time.timeScale = 0f;
-            GameIsPaused = true;
-        }
+    public override void OnNetworkSpawn()
+    {
+        var inputs = FindObjectsOfType<PlayerInput>();
 
-        public void LoadMenu()
+        foreach (var input in inputs)
         {
-            Debug.Log("Loading Menu...");
-            Time.timeScale = 1f;
-            SceneManager.LoadScene("MainMenu");
-        }
-        public void QuitGame()
-        {
-            Debug.Log("Quitting Game...");
-            #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-            #endif
-            Application.Quit(0);
+            Debug.Log(1);
+            if (input.currentActionMap.name == "PauseMenu") continue;
+            Debug.Log(2);
             
+            _actionMaps.Add(input.currentActionMap);
         }
+
+    }
+    public void HandlePauseInput(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        
+        if (_isPaused)
+        {
+            RequestResumeServerRPC();
+        }
+        else
+        {
+            RequestPauseServerRPC();
+        }
+    }
+    
+    [ServerRpc(RequireOwnership = false)]
+    public void RequestResumeServerRPC()
+    {
+        ResumeClientRPC();
+    }
+    
+    [ClientRpc]
+    private void ResumeClientRPC()
+    {
+        EnableInputs();
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        _isPaused = false;
+    }
+    
+    [ServerRpc(RequireOwnership = false)]
+    private void RequestPauseServerRPC()
+    {
+        PauseClientRPC();
+    }
+    
+    [ClientRpc]
+    private void PauseClientRPC()
+    {
+        DisableInputs();
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        _isPaused = true;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void LoadMenuServerRPC()
+    {
+        RestoreTimeClientRPC();
+        ServerGameNetPortal.Instance.EndGame();
+    }
+
+    [ClientRpc]
+    private void RestoreTimeClientRPC()
+    {
+        Time.timeScale = 1f;
+    }
+
+    private void DisableInputs()
+    {
+        foreach (var map in _actionMaps)
+        {
+            map.Disable();
+        }
+    }
+    
+    private void EnableInputs()
+    {
+        foreach (var map in _actionMaps)
+        {
+            map.Enable();
+        }
+    }
 }
 
